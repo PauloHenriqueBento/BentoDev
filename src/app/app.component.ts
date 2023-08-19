8760aed14a423c5436180d37ccc5287988414670
PauloHenriqueBentoDosSantos <phbento@outlook.com.br>
2023-08-18 22:27:28 -0300

    chore: aviso de comming soon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <app-mouse-animation></app-mouse-animation>
+    <div style="height: 95vh; width: 100%; display: flex; justify-content: center; align-items: center;">
+      <h1 style="color: white; font-size: 60px;">coming soon</h1>
+    </div>
     <router-outlet></router-outlet>
   `,
   styleUrls: ['./app.component.scss']
