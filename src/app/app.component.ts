4e25b9dad67c7989a4ed2c8ea3b97827a0deff73
PauloHenriqueBentoDosSantos <phbento@outlook.com.br>
2023-08-19 01:18:43 -0300

    style: created banner to home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <app-mouse-animation></app-mouse-animation>
-    <app-header></app-header>
-    <div style="height: 95vh; width: 100%; display: flex; justify-content: center; align-items: center;">
-      <h1 style="color: white; font-size: 60px;">coming soon</h1>
-    </div>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MouseAnimationComponent } from './components/template/mouse-animation/mouse-animation.component';
 import { HeaderComponent } from './components/template/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BannerComponent } from './components/home/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MouseAnimationComponent,
     HeaderComponent,
+    HomeComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule,
